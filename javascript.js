289af6ea2b8934b92466ee4b2fbb04fc6e44ae1f
RainXuZhang <rainxuzhang@gmail.com>
2025-03-28 12:55:27 -0700

    Removed GTM

--- javascript.js
+++ javascript.js
@@ -10,23 +10,3 @@ tabLinks.forEach(link => {
         document.getElementById(link.getAttribute('data-tab')).classList.add('active');
     });
 });
-
-//Google tag (gtag.js)
-(function() {
-    const script = document.createElement('script');
-    script.async = true;
-    script.src = "https://www.googletagmanager.com/gtag/js?id=G-1GSMZ9NY9Z";
-    document.head.appendChild(script);
-
-    window.dataLayer = window.dataLayer || [];
-    function gtag() { dataLayer.push(arguments); }
-    gtag('js', new Date());
-    gtag('config', 'G-1GSMZ9NY9Z');
-})();
-
-//Google tag manager
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-M2FN56SM');
